3c6bff5977f9d0572698a82d929d90edb6d1f173
Balasubramani S <sbalasubramani23784@gmail.com>
2026-09-21 17:57:40 +0530

    ui added on AppHost

--- USS.AppHost/AppHost.cs
+++ USS.AppHost/AppHost.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.uss_api>("uss-api");
 
+builder.AddProject<Projects.uss_ui>("uss-ui");
+
 builder.Build().Run();
--- uss.ui/Program.cs
+++ uss.ui/Program.cs
@@ -9,6 +9,7 @@ namespace uss.ui
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.AddServiceDefaults();
 
             // Add services to the container.
             builder.Services.AddRazorComponents()
@@ -19,6 +20,7 @@ namespace uss.ui
 
             builder.Services.AddScoped<ICompanyService, CompanyService>();
             var app = builder.Build();
+            app.MapDefaultEndpoints();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
